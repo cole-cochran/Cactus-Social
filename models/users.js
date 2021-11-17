3fb5f4037e0eddfac63d738e33a384dec7348a90
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-16 18:51:41 -0600

    sweet baby rays on shelf

--- models/users.js
+++ models/users.js
@@ -82,7 +82,7 @@ userSchema.pre('save', function(next) {
      });
     });
    });
-   UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+   userSchema.methods.comparePassword = function(candidatePassword, cb) {
      bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
       if (err) return cb(err);
       cb(null, isMatch);
