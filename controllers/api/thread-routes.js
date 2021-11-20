9c9bc26f81efa0988fde391c734a78dc46c32c58
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-20 15:40:11 -0600

    yes

--- controllers/api/thread-routes.js
+++ controllers/api/thread-routes.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
-const mongoose = require('mongoose');
-const db = require('../models')
-const path = require('path');
+const db = require('../../models')
 
+router.get('/', async (req,res)=>{
+    try{
+        const resData=await db.Thread.find({}).populate()
+
+        if(resData.length){
+            console.log('fuckupsgonnafuckup');
+        }
+        res.status(200).json(resData);
+    }catch(err){
+        res.status(400).json(err);
+    }
+   
+});
+
+router.post('/addThread', async({body},res)=>{
+try{
+const resData=await db.Thread.create(body)
+if(resData.length){
+console.log('fuck nougat')
+}
+res.status(200).json(resData)
+}catch(err){
+res.status(400).json(err)
+}
+});
 
 
 module.exports=router
