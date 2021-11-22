81548d6ee2f8c2b80f0787dce381e0f9f8937f1c
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-21 19:25:55 -0600

    added a few packages implemented passport auth and express session

--- controllers/api/pin-routes.js
+++ controllers/api/pin-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const mongoose = require('mongoose');
-const db = require('../models')
+const db = require('../../models')
 const path = require('path');
 
 
