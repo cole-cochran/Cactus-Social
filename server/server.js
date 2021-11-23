6b8bc055291fd0cbab3ab5f6d5f959fba6a27110
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-22 18:21:57 -0600

    added morgan moved setup connection.js

--- server/server.js
+++ server/server.js
@@ -2,26 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const compression = require("compression");
 const routes = require("./controllers/index");
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const morgan=require('morgan')
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+  });
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+server.applyMiddleware({ app });
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cactus_social', {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+
 
 // routes
 app.use(routes);
 
-app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}!`);
-});
+db.once('open', () => {
+	app.listen(PORT, () => {
+	  console.log(`API server running on port ${PORT}!`);
+	  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+	});
+  });
