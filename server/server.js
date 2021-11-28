bb41019dc08529162b0129c6bf8ae818bc9e58ce
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-27 20:37:46 -0600

    no mo problems

--- server/server.js
+++ server/server.js
@@ -1,49 +1,55 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-const compression = require('compression');
+const compression = require("compression");
+// const routes = require("./controllers/index");
+const { ApolloServer } = require('apollo-server-express');
 
-const logger = require("morgan");
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+//const logger = require("morgan");
+
 const path =require('path');
 
-//* Get ApolloServer and grab typeDefs and resolvers from schemas
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-
-//* grab express routes from the controller
-const routes = require('./controllers/index');
-
-//* set up port and express app
-const PORT = process.env.PORT || 3000;
-const app = express();
-
-//* set up the Apollo Server with the typeDefs and resolvers
 const server = new ApolloServer({
 	typeDefs,
-	resolvers
-});
+	resolvers,
+  });
 
-app.use(logger("dev"));
+// const mongoose = require('mongoose');
 
-//* link apollo server to express app
+//* Get ApolloServer and grab typeDefs and resolvers from schemas
+
+//* grab express routes from the controller
+// const routes = require('./controllers/index');
+
+//* set up port andexpress app
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//app.use(logger("dev"));
+
 server.applyMiddleware({ app });
 
-//* set up application middleware
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.static('public'));
 
-// //* connect to mongodb database
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cactus_social', {
-// 	useNewUrlParser: true,
-// 	useFindAndModify: false,
-// 	useUnifiedTopology: true,
-// 	useCreateIndex: true
-// });
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static(path.join(__dirname, '../client/build')));
+//   }
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
+
+//* connect to mongodb database
 
 //* set up middleware for routes
-app.use(routes);
+// app.use(routes);
 
 //* set up server to listen on port and open connection to graphql
 db.once('open', () => {
