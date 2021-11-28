7296ca91710a9d02e9d5d8307315a451f8bb6bda
jack <jackmanzer88@gmail.com>
2021-11-28 12:18:07 -0600

    refactored app structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './jactus-components/Dashboard';
+import Profile from './jactus-components/Profile'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/signup" element={<Dashboard/>}/>
+            <Route path="/login" element={<Dashboard/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+    </Router>
   );
 }
 
