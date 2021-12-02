76c3b2550b029d86da3a9f853c223c5b0fe96452
Cole Cochran <colecochran405@gmail.com>
2021-12-01 19:46:44 -0600

    importing the NavBar

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,5 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import Sidebar from "../components/Sidebar";
-import Footer from "../components/Footer";
-import ProfileDisplay from "../components/ProfileDisplay";
 
 
 export default function Error() {
