8027f7c4b01542f1a222e1470c947c589eb504d9
Cole Cochran <colecochran405@gmail.com>
2021-12-03 11:40:21 -0600

    linking index.js

--- client/src/pages/Sendbird/index.js
+++ client/src/pages/Sendbird/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import Sendbird from "./Sendbird";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Sendbird />, rootElement);
