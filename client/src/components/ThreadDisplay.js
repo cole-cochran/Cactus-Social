93d37fb67d1c7a1e7bb37d668853aaa0317744d5
jack <jackmanzer88@gmail.com>
2021-12-06 15:13:12 -0600

    made chats scrollable and made app height expandable

--- client/src/components/ThreadDisplay.js
+++ client/src/components/ThreadDisplay.js
@@ -2,40 +2,118 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 
+const styles = {
+    
+};
+
 function ThreadDisplay(props) {
     return (
         <main className="thread-wrapper">
-            <div className="thread-content-container">
-                <div className="thread-header">
-                    <h3>Austin Code Bootcamp Students</h3>
-                    <p>M: Damien</p>
-                </div>
-                <div className="chats-container">
-                    <div className="chat">
-                        <div>
-                            <span className="chat-name">Jack</span>
-                            <span className="chat-date">November 21st at 8:40pm</span>
-                        </div>
-                         <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                <div className="thread-content-container">
+                    <div className="thread-header">
+                        <h3>Austin Code Bootcamp Students</h3>
+                        <p>M: Damien</p>
                     </div>
-                    <div className="chat">
-                        <div>
-                            <span className="chat-name">Jack</span>
-                            <span className="chat-date">November 21st at 8:40pm</span>
+                    <div className="chats-container">
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
                         </div>
-                         <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
+                        <div className="chat">
+                            <div>
+                                <span className="chat-name">Jack</span>
+                                <span className="chat-date">November 21st at 8:40pm</span>
+                            </div>
+                            <p>I'm baby seitan health goth wayfarers, succulents direct trade microdosing ethical drinking vinegar chia. Flannel chambray gluten-free sustainable letterpress echo </p>
+                        </div>
                     </div>
-                </div>
-                {/* <div class="add-chat"> */}
-                {/* <div
-                    className="input" 
-                    // role="textbox" 
-                    contenteditable>
-                </div>    */}
-                <TextField multiline style={{maxHeight: 80}} />  
-                    {/* <input type="text" placeholder="Message the thread" contenteditable="true"/> */}
-                {/* </div> */}
-            </div>
+                    <div className="chat-input">
+                        <span contenteditable="true">"what's on your mind?"</span>
+                        <div className="chat-input-buttons">
+                            <button className="chat-input-send-button">send</button>
+                        </div>
+                    </div>
+                 </div>
         </main>
     )
 }
