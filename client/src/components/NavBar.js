c29bff2c3e7b979f0832f7a378ba97cd4941aa34
Cole Cochran <colecochran405@gmail.com>
2021-12-07 13:47:33 -0600

    changing from DM's to messages

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,8 +12,8 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Logout', 'Home', 'Threads', "DM's"];
-const settings = ['Logout', 'Home', 'Threads', "DM's"];
+const pages = ['Logout', 'Home', 'Threads', "Messages"];
+const settings = ['Logout', 'Home', 'Threads', "Messages"];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
