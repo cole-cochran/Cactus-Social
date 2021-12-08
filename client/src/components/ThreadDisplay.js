c5677cc7a3ef20e406f12af5b405f1b1b82fc8bc
jack <jackmanzer88@gmail.com>
2021-12-07 20:50:18 -0600

    added subthread creation modal

--- client/src/components/ThreadDisplay.js
+++ client/src/components/ThreadDisplay.js
@@ -24,9 +24,7 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
 };
 
 
@@ -145,12 +143,16 @@ function ThreadDisplay(props) {
                                 aria-describedby="modal-modal-description"
                             >
                                 <Box sx={style}>
-                                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                                    ext in a modal
-                                    </Typography>
-                                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                                    </Typography>
+                                    <form className="modal-form">
+                                        <div className="modal-header">
+                                            <h4>Add Subthread</h4>
+                                        </div>
+                                        <label>Pin Title</label>
+                                        <input placeholder="e.g. Cactus Party"/>
+                                        <label>Pin Hash</label>
+                                        <input placeholder="e.g. #cactus-party"/>
+                                        <button className="modal-button" type="submit">Add</button>
+                                    </form>
                                 </Box>
                              </Modal>
                         </div>
